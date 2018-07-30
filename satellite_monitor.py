6f428ff8efd88e22a6e45263585bed20faefe2a9
Taft Sanders <tasander@redhat.com>
2018-07-30 15:35:40 -0400

    added ps output

--- satellite_monitor.py
+++ satellite_monitor.py
@@ -62,248 +62,272 @@ class Satellite_Monitor():
 
     def get_Pulp_Tasks(self):
         """Gather all Pulp Tasks"""
+        print('Gather all Pulp Tasks')
         command = ['pulp-admin', '-u', 'admin', '-p', self.pulp_pw,
                      'tasks', 'list']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         pulptasks = output.stdout.read()
-        print('Gather all Pulp Tasks')
         self.write_to_file('pulp_tasks', pulptasks)
 
     def get_Pulp_Status(self):
         """Gather Pulp status"""
+        print('Gather Pulp status')
         command = ['pulp-admin', '-u', 'admin', '-p', self.pulp_pw, 'status']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         pulpstatus = output.stdout.read()
-        print('Gather Pulp status')
         self.write_to_file('pulp_status', pulpstatus)
 
     def get_Celery_Active_Tasks(self):
         """Gather all active celery tasks"""
+        print('Gather all active celery tasks')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'active']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         ctasks = output.stdout.read()
-        print('Gather all active celery tasks')
         self.write_to_file('celery_tasks_active', ctasks)
 
     def get_Celery_Scheduled_Tasks(self):
         """Gather all scheduled celery tasks"""
+        print('Gather all scheduled celery tasks')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'scheduled']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cschtasks = output.stdout.read()
-        print('Gather all scheduled celery tasks')
         self.write_to_file('celery_tasks_scheduled', cschtasks)
 
     def get_Celery_Reserved_Tasks(self):
         """Gather all reserved celery tasks"""
+        print('Gather all reserved celery tasks')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'reserved']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         crsvptasks = output.stdout.read()
-        print('Gather all reserved celery tasks')
         self.write_to_file('celery_tasks_reserved', crsvptasks)
 
     def get_Celery_Revoked_Tasks(self):
         """Gather all revoked celery tasks"""
+        print('Gather all revoked celery tasks')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'revoked']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         crevoketasks = output.stdout.read()
-        print('Gather all revoked celery tasks')
         self.write_to_file('celery_tasks_revoked', crevoketasks)
 
     def get_Celery_Registered_Tasks(self):
         """Gather all registered celery tasks"""
+        print('Gather all registered celery tasks')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'registered']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cregtasks = output.stdout.read()
-        print('Gather all registered celery tasks')
         self.write_to_file('celery_tasks_registered', cregtasks)
 
     def get_Celery_Stats(self):
         """Gather celery stats"""
+        print('Gather celery stats')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'stats']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cstats = output.stdout.read()
-        print('Gather celery stats')
         self.write_to_file('celery_stats', cstats)
 
     def get_Celery_Active_Queues(self):
         """Gather celery active queues"""
+        print('Gather celery active queues')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'active_queues']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cactiveques = output.stdout.read()
-        print('Gather celery active queues')
         self.write_to_file('celery_active_queues', cactiveques)
 
     def get_Celery_Clock(self):
         """Gather celery clocks"""
+        print('Gather celery clocks')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'clock']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cclocks = output.stdout.read()
-        print('Gather celery clocks')
         self.write_to_file('celery_clock', cclocks)
     
     def get_Celery_Conf(self):
         """Gather celery configs"""
+        print('Gather celery configs')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'conf']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cconf = output.stdout.read()
-        print('Gather celery configs')
         self.write_to_file('celery_conf', cconf)
 
     def get_Celery_Memory_Dump(self):
         """Gather celery memory dump"""
+        print('Gather celery memory dump')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'memdump']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cmemdump = output.stdout.read()
-        print('Gather celery memory dump')
         self.write_to_file('celery_memory_dump', cmemdump)
 
     def get_Celery_Memory_Sample(self):
         """Gather celery memory sample"""
+        print('Gather celery memory sample')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'memsample']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cmemsamp = output.stdout.read()
-        print('Gather celery memory sample')
         self.write_to_file('celery_memory_sample', cmemsamp)
 
     def get_Celery_Obj_Graph(self):
         """Gather celery object graph"""
+        print('Gather celery object graph')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'objgraph']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cobjgraph = output.stdout.read()
-        print('Gather celery object graph')
         self.write_to_file('celery_objgraph', cobjgraph)
 
     def get_Celery_Ping(self):
         """Gather celery ping"""
+        print('Gather celery ping')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 'ping']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cping = output.stdout.read()
-        print('Gather celery ping')
         self.write_to_file('celery_ping', cping)
 
     def get_Celery_Report(self):
         """Gather celery bugreport"""
+        print('Gather celery bugreport')
         command = ['celery', '-A', 'pulp.server.async.app', 'inspect', 
                     'report']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         cbug = output.stdout.read()
-        print('Gather celery bugreport')
         self.write_to_file('celery_bugreport', cbug)
 
     def get_Qpid_General(self):
         """Gather Qpid General"""
+        print('Gather Qpid General')
         command = ['qpid-stat', '-g', '--ssl-certificate=\
                     /etc/pki/katello/qpid_client_striped.crt', 
                     '-b', 'amqps://localhost:5671']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         qpidgen = output.stdout.read()
-        print('Gather Qpid General')
         self.write_to_file('qpid_general', qpidgen)
 
     def get_Qpid_Connections(self):
         """Gather Qpid Connections"""
+        print('Gather Qpid Connections')
         command = ['qpid-stat', '-c', '--ssl-certificate=\
                     /etc/pki/katello/qpid_client_striped.crt', 
                     '-b', 'amqps://localhost:5671']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         qpidconn = output.stdout.read()
-        print('Gather Qpid Connections')
         self.write_to_file('qpid_connections', qpidconn)
 
     def get_Qpid_Exchanges(self):
         """Gather Qpid Exchanges"""
+        print('Gather Qpid Exchanges')
         command = ['qpid-stat', '-e', '--ssl-certificate=\
                     /etc/pki/katello/qpid_client_striped.crt', 
                     '-b', 'amqps://localhost:5671']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         qpidexch = output.stdout.read()
-        print('Gather Qpid Exchanges')
         self.write_to_file('qpid_exchanges', qpidexch)
 
     def get_Qpid_Queues(self):
         """Gather Qpid Queues"""
+        print('Gather Qpid Queues')
         command = ['qpid-stat', '-q', '--ssl-certificate=\
                     /etc/pki/katello/qpid_client_striped.crt', 
                     '-b', 'amqps://localhost:5671']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         qpidque = output.stdout.read()
-        print('Gather Qpid Queues')
         self.write_to_file('qpid_queue', qpidque)
 
     def get_Qpid_Subscriptions(self):
         """Gather Qpid Subscriptions"""
+        print('Gather Qpid Subscriptions')
         command = ['qpid-stat', '-u', '--ssl-certificate=\
                     /etc/pki/katello/qpid_client_striped.crt', 
                     '-b', 'amqps://localhost:5671']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         qpidsub = output.stdout.read()
-        print('Gather Qpid Subscriptions')
         self.write_to_file('qpid_subscriptions', qpidsub)
 
     def get_Qpid_Memory(self):
         """Gather Qpid Memory"""
+        print('Gather Qpid Memory')
         command = ['qpid-stat', '-m', '--ssl-certificate=\
                     /etc/pki/katello/qpid_client_striped.crt', 
                     '-b', 'amqps://localhost:5671']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         qpidmem = output.stdout.read()
-        print('Gather Qpid Memory')
         self.write_to_file('qpid_memory', qpidmem)
 
     def get_Mongo_Tasks(self):
         """Gather Mongodb Tasks"""
+        print('Gather Mongodb Tasks')
         command = ['mongoexport', '--db', 'pulp_database', '--collection',
                     'task_status']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         mongotasks = output.stdout.read()
-        print('Gather Mongodb Tasks')
         self.write_to_file('mongo_tasks', mongotasks)
 
     def get_Mongo_RSVP_Resource(self):
         """Gather Mongodb Reserved Resources"""
+        print('Gather Mongodb Reserved Resources')
         command = ['mongoexport', '--db', 'pulp_database', '--collection',
                     'reserved_resources']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         mongoresources = output.stdout.read()
-        print('Gather Mongodb Reserved Resources')
         self.write_to_file('mongo_resources', mongoresources)
 
     def get_Mongo_Workers(self):
         """Gather Mongodb Workers"""
+        print('Gather Mongodb Workers')
         command = ['mongoexport', '--db', 'pulp_database', '--collection',
                     'workers']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         mongoworkers = output.stdout.read()
-        print('Gather Mongodb Workers')
         self.write_to_file('mongo_workers', mongoworkers)
 
     def get_Passenger_Status(self):
         """Gather Passenger Statistics"""
+        print('Gather Passenger Status')
         command = ['passenger-status']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         passengerstats = output.stdout.read()
-        print('Gather Passenger Status')
         self.write_to_file('passenger-status', passengerstats)
 
     def get_Passenger_Memory(self):
         """Gather Passenger Memory Usage"""
+        print('Gather Passenger Memory Usage')
         command = ['passenger-memory-stats']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         passengermem = output.stdout.read()
-        print('Gather Passenger Memory Usage')
         self.write_to_file('passenger_memory', passengermem)
+
+    def get_Process_Output1(self):
+        """Gather ps_alxwww Output"""
+        print('Gather ps_alxwww Output')
+        command = ['ps', 'alxwww']
+        output = subprocess.Popen(command, stdout=subprocess.PIPE)
+        ps_alxwww = output.stdout.read()
+        self.write_to_file('ps_alxwww', ps_alxwww)
+
+    def get_Process_Output2(self):
+        """Gather ps_auxwww Output"""
+        print('Gather ps_auxwww Output')
+        command = ['ps', 'auxwww']
+        output = subprocess.Popen(command, stdout=subprocess.PIPE)
+        ps_auxwww = output.stdout.read()
+        self.write_to_file('ps_auxwww', ps_auxwww)
+
+    def get_Process_Output3(self):
+        """Gather ps_auxwwwm Output"""
+        print('Gather ps_auxwwwm Output')
+        command = ['ps', 'auxwwwm']
+        output = subprocess.Popen(command, stdout=subprocess.PIPE)
+        ps_auxwwwm = output.stdout.read()
+        self.write_to_file('ps_auxwwwm', ps_auxwwwm)
 
     def get_SAR_data(self):
         if not os.path.exists(SAR):
@@ -371,7 +395,10 @@ class Satellite_Monitor():
             get_Mongo_Tasks,
             get_Mongo_Workers,
             get_Passenger_Memory,
-            get_Passenger_Status]
+            get_Passenger_Status,
+            get_Process_Output1,
+            get_Process_Output2,
+            get_Process_Output3]
 
 def main():
     #pdb.set_trace()
