8e386163a1e3bd677004725c3af23e8c3f9a4641
Taft Sanders <tasander@redhat.com>
2018-08-03 16:08:04 -0400

    working on interactive register

--- interactive_register.py
+++ interactive_register.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import os
+import gps_satellite as gps
 
 class Interactive_Register():
 
@@ -14,8 +15,11 @@ class Interactive_Register():
         if str(dest) == '1':
             print('Registering to the Customer Portal...')
             self.register()
+            self.get_subscriptions()
         elif str(dest) == '2':
             print('Registering to a Satellite...')
+            self.get_latest_ca_consumer_rpm()
+            self.register()
         elif str(dest) == '3':
             exit
         else:
@@ -24,13 +28,27 @@ class Interactive_Register():
 
     def register(self):
         command = 'sudo subscription-manager register'
-        output = subprocess.Popen(command, shell=True)
-        output.wait()
+        reg = subprocess.Popen(command, shell=True)
+        reg.wait()
+
+    def get_latest_ca_consumer_rpm(self):
+        get_fqdn = raw_input('What is the fully qaulified domain name of your Satellite server?\n' +
+                             'Example: satellite.example.redhat.com ')
+        command = 'rpm -Uvh http://' + get_fqdn + '/pub/katello-ca-consumer-latest.noarch.rpm'
+        reg = subprocess.Popen(command, shell=True)
+        reg.wait()
+    
+    def get_Sat_Org(self):
+        
 
     def get_subscriptions(self):
         command = ['subscription-manager', 'list', '--available']
         output = subprocess.Popen(command, stdout=subprocess.PIPE)
         subscriptions = output.stdout.read()
+        sub1 = subscriptions.strip()
+        #print(sub1)
+        sub2 = sub1.split('\n')
+        print(sub2)
         
 def main():
     register = Interactive_Register()
